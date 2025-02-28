1438adae22f527e014377deab93c5790e3a1448a
SADTOPIA <116076264+SADTOPIA@users.noreply.github.com>
2025-02-28 19:08:23 +0200

    remove comments

--- store/page.js
+++ store/page.js
@@ -1,19 +1,3 @@
-// import {configureStore} from "@reduxjs/toolkit";
-//
-//
-// import charactersSlice from './characters-list-slice';
-// import characterItemSlice  from './character-item-slice';
-// import filterSlice from './filter-slice';
-//
-// const store = configureStore({
-//   reducer: {
-//     characters: charactersSlice.reducer,
-//     characterItem: characterItemSlice.reducer,
-//     filter: filterSlice.reducer,
-//   }
-// });
-// export default store;
-
 import { configureStore } from "@reduxjs/toolkit";
 import charactersReducer from "./characters-list-slice";
 import characterItemReducer from "./character-item-slice";
